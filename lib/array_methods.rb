895212a958e15d2b82f39ba6f088b756abe46626
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-06 00:02:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,6 @@
 def find_element_index(array, value_to_find)
- counter = 0 
- 
- while counter < array.length do 
-   if array[counter] == value_to_find
-     return counter
-   end
-   counter += 1 
- end
- nil
+ array.length.times do |val|
+   
 end
 
 def find_max_value(array)
