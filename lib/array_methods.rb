c06654c77e6a7cdbd485ee01c6354633a02d6874
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-06 00:05:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@ end
 def find_max_value(array)
   counter = 0 
   
-  array.length.times { |idx| if array[idx] 
+  array.length.times { |idx| if array[idx] > counter 
 end
 
 def find_min_value(array)
